c777123c79d2e8e49eadeaba2554ffa548d34674
hvgochr <95345156+hvgochr@users.noreply.github.com>
2022-01-11 17:34:58 +0100

    Update App.java

--- App.java
+++ App.java
@@ -36,7 +36,23 @@ public class App {
         System.out.println("----");
 
         //Test ToString
-        System.out.println(testHTNaive.toString());
+        System.out.println("toString HTNaive" + testHTNaive.toString());
+        System.out.println(testHTNaive.getNBListes());
+        System.out.println(testHTNaive.getCardinal());
+        System.out.println(testHTNaive.getMaxSize());
+        System.out.println(testHTNaive.toStringV2());
+        
+        //Test
+        double f = 1/2;
+        HTNaive test2HtNaive = new HTNaive(addListe, f);
+        System.out.println("toStringV2 HTNaive" + test2HtNaive.toStringV2());
+
+        Dictionnaire dic = new Dictionnaire(5);
+        System.out.println("affichage avant ajout zbq" + dic.toStringV2());
+        System.out.println("true si zbq ajouté"+dic.ajout("zbq"));
+        System.out.println("affichage après ajout zbq" + dic.toStringV2());
+
+
     }
 
 }
