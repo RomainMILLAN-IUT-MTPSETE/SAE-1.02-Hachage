6b4dc8b3aef8759fb45db71e47520c99933ff9f0
= <=>
2022-01-11 19:30:13 +0100

    Modified of Dictionnaire.java

--- Dictionnaire.java
+++ Dictionnaire.java
@@ -15,6 +15,22 @@ public class Dictionnaire {
     }
 
     /**
+     * A: Constructeur de la classe
+     * @param filename
+     * @param m
+     */
+    public Dictionnaire(String filename, int m){
+        this.list = new HTNaive(calculeListeInt(filename), m);
+    }
+
+    /**
+     * A: Constructeur de la classe
+     */
+    public Dictionnaire(String filename, double f){
+        this.list = new HTNaive(calculeListeInt(filename), lectureMotsTexte(filename)*f);
+    }
+
+    /**
      * A/R: Convertie un String en int avec les valeurs ascii.
      * @param s
      * @return
@@ -82,5 +98,78 @@ public class Dictionnaire {
     public String toStringV2(){
         return this.list.toStringV2();
     }
+
+    /**
+     * 
+     * @return
+     */
+    public int lectureMotsTexte(String filename){
+        //on suppose que fichier.txt est un fichier dans le meme dossier
+        //que les .java
+        File f = new File(filename);
+        ListeBigI res = new ListeBigI();
+        Scanner sc;
+        //un scanner est un objet permettant de "scanner" (parcourir)
+        //une entrée (clavier, ou une chaîne, ou un File, etc)
+        try {
+            sc = new Scanner(f);
+            //ici on construit le scanner avec comme entrée f
+            //cette construction peut échouer (si par exemple fichier.txt n’existe pas)
+        }catch(FileNotFoundException e){
+            //si la construction échoue, on passe ici
+            System.out.println(("problème d’accès au fichier " + e.getMessage()));
+            return 0;
+        }
+        sc.useDelimiter(" |\\n|,|;|:|\\.|!|\\?|-");
+        //on définit les délimiteurs comme le caractère ’\n’, le caractère ’,’ etc...
+        //cela définit maintenant la notion de "morceau" comme une suite
+        //de caractères entre deux délimiteurs
+        int nbmots = 0;
+        while (sc.hasNext()) { //sc.hasNext() renvoie vrai ssi
+            //il reste encore un morceau à découvrir dans f
+            String mot = sc.next(); //sc.next() renvoie le prochain morceau
+            nbmots ++;
+        }
+        return nbmots;
+    }
+
+    public static ListeBigI calculeListeInt(String fileName){
+        ListeBigI resultat = new ListeBigI();
+
+        //on suppose que fichier.txt est un fichier dans le meme dossier
+        //que les .java
+        File f = new File(fileName);
+        ListeBigI res = new ListeBigI();
+        Scanner sc;
+        //un scanner est un objet permettant de "scanner" (parcourir)
+        //une entrée (clavier, ou une chaîne, ou un File, etc)
+        try {
+            sc = new Scanner(f);
+            //ici on construit le scanner avec comme entrée f
+            //cette construction peut échouer (si par exemple fichier.txt n’existe pas)
+        }catch(FileNotFoundException e){
+            //si la construction échoue, on passe ici
+            System.out.println(("problème d’accès au fichier " + e.getMessage()));
+            return 0;
+        }
+        sc.useDelimiter(" |\\n|,|;|:|\\.|!|\\?|-");
+        //on définit les délimiteurs comme le caractère ’\n’, le caractère ’,’ etc...
+        //cela définit maintenant la notion de "morceau" comme une suite
+        //de caractères entre deux délimiteurs
+        int nbmots = 0;
+        while (sc.hasNext()) { //sc.hasNext() renvoie vrai ssi
+            //il reste encore un morceau à découvrir dans f
+            String mot = sc.next(); //sc.next() renvoie le prochain morceau
+            if(resultat.getTete() == null){
+                resultat.ajoutTete(stringToBigInteger(mot));
+            }else {
+                resultat.ajoutFin(stringToBigInteger(mot));
+            }
+        }
+
+        return resultat;
+    }
+
+
 
 }
